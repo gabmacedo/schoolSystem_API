2274392436f210748780457b8932f3d65adeedcc
Gabriel Macedo <gabmacedo33@gmail.com>
2025-05-05 15:52:13 -0300

    fix: endpoints para deploy no render e configurações do swagger

--- professor/professor_model.py
+++ professor/professor_model.py
@@ -4,21 +4,15 @@ from datetime import date
 class Professor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    data_nascimento = db.Column(db.String(10), nullable=False)
-    disciplina = db.Column(db.String(100), nullable=False)
-    salario = db.Column(db.Float, nullable=False)
-
-    def calcular_idade(self):
-        ano, mes, dia = map(int, self.data_nascimento.split('-'))
-        hoje = date.today()
-        idade = hoje.year - ano - ((hoje.month, hoje.day) < (mes, dia))
-        return idade
+    materia = db.Column(db.String(100), nullable=False)
+    observacoes = db.Column(db.String(255), nullable=True)
+    idade = db.Column(db.Integer, nullable=False)
 
     def to_dict(self):
         return {
             "id": self.id,
             "nome": self.nome,
-            "idade": self.calcular_idade(),
-            "disciplina": self.disciplina,
-            "salario": self.salario
+            "materia": self.materia,
+            "observacoes": self.observacoes,
+            "idade": self.idade
         }
